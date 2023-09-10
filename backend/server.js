37033475cf9794a6467ce1cfa5202d3e1bb85369
“Lesley <you@example.com>
2023-09-09 20:31:29 -0400

    3rd attempt

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://travel-tracker-e3l8.onrender.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if ('OPTIONS' == req.method) {
